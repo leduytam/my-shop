90f9c313a8addef2eca42e252f4556ae3055c58b
Bành Hảo Toàn <86637323+mjkundta@users.noreply.github.com>
2023-05-02 02:36:23 +0700

    do some small fix

--- MyShop/Models/DAL/OrderDAO.cs
+++ MyShop/Models/DAL/OrderDAO.cs
@@ -342,31 +342,37 @@ namespace MyShop.Models.DAL
         }
         public List<int> GetNumberOfBookSoldByDay(string bookName, DateTime fromDate, DateTime toDate)
         {
-            var bookSoldByDay = _dbContext.OrderItems
+            using (var dbContext = new MyShopDbContext())
+            {
+                var bookSoldByDay = dbContext.OrderItems
                 .Where(oi => oi.Book.Name == bookName && oi.Order.CreatedAt >= fromDate && oi.Order.CreatedAt <= toDate)
                 .GroupBy(oi => oi.Order.CreatedAt.Date)
                 .Select(g => g.Sum(oi => oi.Quantity))
                 .ToList();
 
-            return bookSoldByDay;
+                return bookSoldByDay;
+            }
         }
         public List<int> GetBookSalesByDay(string bookName, DateTime startDate, DateTime endDate)
         {
-            List<int> salesByDay = new List<int>();
-
-            // Loop through each day within the date range
-            for (DateTime date = startDate.Date; date <= endDate.Date; date = date.AddDays(1))
+            using (var _dbContext = new MyShopDbContext())
             {
-                // Count the number of books sold for the given book name on this day
-                int booksSold = _dbContext.OrderItems
-                    .Where(oi => oi.Book.Name == bookName && oi.Order.CreatedAt.Date == date)
-                    .Sum(oi => oi.Quantity);
+                List<int> salesByDay = new List<int>();
 
-                // Add the number of books sold to the salesByDay list
-                salesByDay.Add(booksSold);
+                // Loop through each day within the date range
+                for (DateTime date = startDate.Date; date <= endDate.Date; date = date.AddDays(1))
+                {
+                    // Count the number of books sold for the given book name on this day
+                    int booksSold = _dbContext.OrderItems
+                        .Where(oi => oi.Book.Name == bookName && oi.Order.CreatedAt.Date == date)
+                        .Sum(oi => oi.Quantity);
+
+                    // Add the number of books sold to the salesByDay list
+                    salesByDay.Add(booksSold);
+                }
+
+                return salesByDay;
             }
-
-            return salesByDay;
         }
 
     }
