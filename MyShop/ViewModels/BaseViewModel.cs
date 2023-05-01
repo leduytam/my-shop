0beb72ac05ccb5bf744e59b1df210953b3a998fc
Sawashi <kietle234.cc@gmail.com>
2023-05-01 20:23:45 +0700

    Update for task 5.2

--- MyShop/ViewModels/BaseViewModel.cs
+++ MyShop/ViewModels/BaseViewModel.cs
@@ -17,5 +17,15 @@ namespace MyShop.ViewModels
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 
         }
+        protected bool SetProperty<T>(ref T backingField, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(backingField, value)) return false;
+            backingField = value;
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
--- MyShop/ViewModels/StastisticsReportViewModel.cs
+++ MyShop/ViewModels/StastisticsReportViewModel.cs
@@ -5,6 +5,8 @@ using MyShop.Models;
 using MyShop.Models.DAL;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,8 @@ namespace MyShop.ViewModels
     public class StastisticsReportViewModel : BaseViewModel
     {
         private OrderDAO? _orderDAO = new OrderDAO();
+        private BookDAO? _bookDAO = new BookDAO();
+
         public Func<double, string> Values { get; set; }
         public List<string>? Dates { get; set; }
         public SeriesCollection SeriesCollection { get; set; }
@@ -24,6 +28,48 @@ namespace MyShop.ViewModels
         public SeriesCollection Collection4 { get; set; }
         public List<string>? Dates4 { get; set; }
         public SeriesCollection SeriesCollection2 { get; set; }
+
+        private SeriesCollection _chartSeries;
+        private SeriesCollection _chartSeries1;
+        private SeriesCollection _chartSeries2;
+        private SeriesCollection _chartSeries3;
+        public SeriesCollection CollectionFromTo {
+            get => _chartSeries;
+            set => SetProperty(ref _chartSeries, value);
+        }
+        public List<string>? DatesFromTo { get; set; }
+        public SeriesCollection CollectionWeek
+        {
+            get => _chartSeries1;
+            set => SetProperty(ref _chartSeries1, value);
+        }
+        public List<string>? DatesWeek { get; set; }
+        public SeriesCollection CollectionMonth
+        {
+            get => _chartSeries2;
+            set => SetProperty(ref _chartSeries2, value);
+        }
+        public List<string>? DatesMonth{ get; set; }
+        public SeriesCollection CollectionYear
+        {
+            get => _chartSeries3;
+            set => SetProperty(ref _chartSeries3, value);
+        }
+        public List<string>? DatesYear { get; set; }
+        public List<Book>? Books { get; set; }
+        private List<string> _items;
+        public List<string> Items
+        {
+            get => _items;
+            set => SetProperty(ref _items, value);
+        }
+
+        private string _selectedItem;
+        public string SelectedItem
+        {
+            get => _selectedItem;
+            set => SetProperty(ref _selectedItem, value);
+        }
         public StastisticsReportViewModel()
         {
             // 7 days revenue
@@ -91,7 +137,98 @@ namespace MyShop.ViewModels
 
             Values = value => value.ToString("C");
 
+            // Get all books
+            Books= new List<Book>();
+            Books = _bookDAO.GetAllBooks();
+            List<String> BookName= new List<String>();
+            for(int i=0;i<Books.Count;i++)
+            {
+                BookName.Add(Books[i].Name);
+            }
+            Items = new List<string>();
+            Items.AddRange(BookName);
+        }
+        public void OnItemSelected()
+        {
+            //Console.WriteLine("Da chay");
+            // Implement the desired action here.
+            // For example, you could show a message box with the selected item:
+            //MessageBox.Show($"Selected item: {SelectedItem}");
+            DatesFromTo = new List<string>();
+            List <int> numberOfBook = new List<int>();
+            var fromDate = new DateTime(2023, 4, 11);
+            var toDate = new DateTime(2023, 4, 30);
 
+            //From 11-4-2023 to 30-04-2023
+            numberOfBook =_orderDAO.GetBookSalesByDay(SelectedItem, fromDate, toDate);
+            CollectionFromTo = new SeriesCollection()
+            {
+                new ColumnSeries    
+                {
+                    Title = "Number of book sold from 11-4-2023 to 30-4-2023",
+                    Values = new ChartValues<int>(numberOfBook)
+                }
+            };
+            for (int i = 19; i >= 0; i--)
+            {
+                DatesFromTo.Add(DateTime.UtcNow.AddDays(-i).ToString("MM-dd"));
+            }
+
+            //Week
+            DatesWeek = new List<string>();
+            
+            DateTime endDateWeek = DateTime.UtcNow.Date; // Get the current UTC date
+            DateTime startDateWeek = endDateWeek.AddDays(-7);
+            numberOfBook = _orderDAO.GetBookSalesByDay(SelectedItem, startDateWeek, endDateWeek);
+            CollectionWeek = new SeriesCollection()
+            {
+                new ColumnSeries
+                {
+                    Title = "Revenue for 7 days",
+                    Values = new ChartValues<int>(numberOfBook)
+                }
+            };
+            for (int i = 6; i >= 0; i--)
+            {
+                DatesWeek.Add(DateTime.UtcNow.AddDays(-i).ToString("MM-dd"));
+            }
+
+            //Month
+            DatesMonth = new List<string>();
+            
+            DateTime endDateMonth = new DateTime(2023, 4, 30);
+            DateTime startDateMonth = new DateTime(2023, 4, 1);
+            numberOfBook = _orderDAO.GetBookSalesByDay(SelectedItem, startDateMonth, endDateMonth);
+            CollectionMonth = new SeriesCollection()
+            {
+                new ColumnSeries
+                {
+                    Title = "Revenue for month 4",
+                    Values = new ChartValues<int>(numberOfBook)
+                }
+            };
+            for (int i = 30; i >= 0; i--)
+            {
+                DatesMonth.Add(DateTime.UtcNow.AddDays(-i).ToString("MM-dd"));
+            }
+            //Year
+            DatesYear = new List<string>();
+            DateTime endDateYear = DateTime.UtcNow.Date; // Get the current UTC date
+            DateTime startDateYear = endDateYear.AddYears(-1).AddDays(1); // Calculate the start date 1 year ago
+            numberOfBook = _orderDAO.GetBookSalesByDay(SelectedItem, startDateYear, endDateYear);
+            CollectionYear = new SeriesCollection()
+            {
+                new ColumnSeries
+                {
+                    Title = "Revenue for last year",
+                    Values = new ChartValues<int>(numberOfBook)
+                }
+            };
+            for (int i = 11; i >= 0; i--)
+            {
+                DatesYear.Add(DateTime.UtcNow.AddMonths(-i).ToString("MM-yyyy"));
+            }
+            //Values = value => value.ToString("C");
         }
     }
 }
